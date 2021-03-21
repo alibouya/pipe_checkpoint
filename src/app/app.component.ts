e47b8254a76aa8abec642757da47a1a35e2d61c4
unknown <ali.bouyahya@gmail.com>
2021-03-21 12:42:28 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipescheck';
-}
+  var1='C:/Users/hp/Desktop/im/'
+  imgURL: any ='';
+
+  imagename='';
+  preview(value){
+
+console.log(value)
+   // this.imagename=this.var1.concat(value.substring(12));
+    //.toString();
+    var reader = new FileReader();
+     this.imagename = value;
+     reader.readAsDataURL(value[0]); 
+     reader.onload = (_event) => { 
+       this.imgURL = reader.result;
+       console.log(this.imgURL)
+
+    
+   }
+}}
